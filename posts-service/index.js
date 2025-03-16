8dc723ae32f88628ceb22e04a969986b8b3ca13c
RubyDEAD <reitzdave.andriano@gmail.com>
2025-03-16 14:09:42 +0800

    updated

--- posts-service/index.js
+++ posts-service/index.js
@@ -13,20 +13,35 @@ const typeDefs = gql`
 
   type Query {
     posts: [Post]
+    post(id: ID!): Post
   }
 
   type Mutation {
     createPost(title: String!, content: String!, userId: Int!): Post
+    updatePost(id: ID!, title: String, content: String): Post
+    deletePost(id: ID!): Post
   }
 `;
 
 const resolvers = {
   Query: {
     posts: () => prisma.post.findMany(),
+    post: (_, { id }) => prisma.post.findUnique({ where: { id: parseInt(id) } }),
   },
   Mutation: {
     createPost: (_, { title, content, userId }) =>
       prisma.post.create({ data: { title, content, userId } }),
+
+    updatePost: (_, { id, title, content }) => 
+      prisma.post.update({
+        where: { id: parseInt(id) },
+        data: { title, content },
+      }),
+
+    deletePost: (_, { id }) => 
+      prisma.post.delete({
+        where: { id: parseInt(id) },
+      }),
   },
 };
 
--- users-service/index.js
+++ users-service/index.js
@@ -12,19 +12,36 @@ const typeDefs = gql`
 
   type Query {
     users: [User]
+    user(id: ID!): User
   }
 
   type Mutation {
     createUser(name: String!, email: String!): User
+    updateUser(id: ID!, name: String, email: String): User
+    deleteUser(id: ID!): User
   }
 `;
 
 const resolvers = {
   Query: {
     users: () => prisma.user.findMany(),
+    user: (_, { id }) => prisma.user.findUnique({ where: { id: parseInt(id) } }),
   },
   Mutation: {
     createUser: (_, { name, email }) => prisma.user.create({ data: { name, email } }),
+    
+    updateUser: async (_, { id, name, email }) => {
+      return prisma.user.update({
+        where: { id: parseInt(id) },
+        data: { name, email },
+      });
+    },
+
+    deleteUser: async (_, { id }) => {
+      return prisma.user.delete({
+        where: { id: parseInt(id) },
+      });
+    },
   },
 };
 
